e2f10376b12fa8f1a9a4915f7248924fcbb82399
hyjoong <maplesosser@naver.com>
2024-01-20 20:14:23 +0900

    feat: next/image domains setting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "d1dsr05o5i286u.cloudfront.net",
+      "d1tftw9jjs9bx5.cloudfront.net",
+      "d2lfdrnysfmya6.cloudfront.net",
+      "d2na9jaypdrp94.cloudfront.net",
+    ],
+    minimumCacheTTL: 2678400,
+    // formats: ["image/avif", "image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
